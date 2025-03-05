ab8267fa690e1145e1a73d5344524349f2925193
Suhani Amnerkar <suhaniamnerkar@gmail.com>
2025-03-05 22:08:25 +0530

    Update Deletion.c

--- Deletion.c
+++ Deletion.c
@@ -4,21 +4,41 @@ struct node{
         int a;
         struct node*next;
     };
+
+
+struct node*create_node(int data){
+    struct node*cnode=(struct node*)malloc(sizeof(struct node*));
+    cnode->a=data;
+    cnode->next=NULL;
+    return cnode;
+}
+
+struct node*input(int data,struct node*head){
+    struct node*nnode=create_node(data);
+    if(head==NULL){
+        head=nnode;
+        return head;
+    }
+    struct node*temp=head;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next=nnode;
+    return head;
+}
 void display(struct node*ptr){
     while(ptr!=NULL){
-        printf("Element: %d\n",ptr->a);
+        printf("%d\n",ptr->a);
         ptr=ptr->next;
     }
 }
 
-void Delete_first(struct node*ptr){
-    struct node*temp;
-    temp=ptr;
-    ptr=ptr->next;
-    free(ptr);
+struct node*delete_at_first(struct node*head){
+    head=head->next;
+    return head;
 }
 
-void Delete_last(struct node*ptr){
+/*void Delete_last(struct node*ptr){
     struct node*temp,*prev;
     temp=ptr;
     while(temp->next!=0){
@@ -28,46 +48,50 @@ void Delete_last(struct node*ptr){
     }
     free(temp);
     prev->next=0;
-}
+}*/
 
-void Delete_at_index(struct node*ptr,int loc){
-    struct node*temp,*prev;
-    temp=ptr;
-    int i=1;
-    while(i<loc){
-        prev=temp;
+struct node*delete_at_index(struct node*head,int m){
+    struct node*temp=head;
+    for(int i=1;i<m-1;i++){
         temp=temp->next;
-        i++;
     }
-    prev->next=temp->next;
-    temp->next=0;
-    free(temp);
+    temp->next=temp->next->next;
+    return head;
 }
 
+struct node*delete_at_last(struct node*head,int n){
+    struct node*temp=head;
+    for(int i=1;i<n-1;i++){
+        temp=temp->next;
+    }
+    temp->next=NULL;
+    return head;
+}
 
 int main(){
-    
-    struct node*head,*first,*second,*third;
-    head=(struct node*)malloc(sizeof(struct node*));
-    first=(struct node*)malloc(sizeof(struct node*));
-    second=(struct node*)malloc(sizeof(struct node*));
-    third=(struct node*)malloc(sizeof(struct node*));
-    head->a=7;
-    head->next=first; 
-    first->a=8;
-    first->next=second;
-    second->a=9;
-    second->next=third;
-    third->a=10;
-    third->next=NULL;
-    int n;
-    display(head);
-    Delete_last(head);
-    display(head);
-    printf("Enter the position of element you want to delete");
+    int n,m;
+    struct node*nnode,*head;
+    head=NULL;
+    int data;
+    printf("Enter the number of elements you want to enter");
     scanf("%d",&n);
-    Delete_at_index(head,n);
-    display(head);
+    for(int i=0;i<n;i++){
+        scanf("%d",&data);
+        head=input(data,head);
+    }
+    printf("Enter the position of element you want to delete:");
+    scanf("%d",&m);
 
-    return 0;
+    if(m==1){
+        head=delete_at_first(head);
+        display(head);
+    }
+    else if(m>1 && m<n){
+        head=delete_at_index(head,m);
+        display(head);
+    }
+    else if(m==n){
+        head=delete_at_last(head,n);
+        display(head);
+    }
 }
